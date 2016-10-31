90a7bdbfc17e2dd83dba7656855f5f0e87eb62ae
Marian Pitel <mpitel@uoguelph.ca>
2016-10-31 19:32:33 -0400

    commit again

--- script_1_confidence-interval-correlation.R
+++ script_1_confidence-interval-correlation.R
@@ -1,6 +1,9 @@
 library(learnSampling)
 library(psych)
 library(ggplot2)
+library(pwr)
+library(predictionInterval)
+
 psych::r.con(r=.21,n=100)
 #0.01416621 0.39031834
 ##means that population correlation can be as low as ... and high as ... 
@@ -50,4 +53,40 @@ print(upper.bound.hist)
 both.hist <- ggplot (ci.upper.bound.correlations,aes(r))
 both.hist <- both.hist + geom_histogram(aes(x= ci.upper.bound.correlations$r, y=..count..),fill="red", binwidth=.05)
 both.hist <- both.hist + geom_histogram(aes(x=ci.lower.bound.correlations$r, y=..count..), fill="blue", binwidth = .05)
-print(both.hist)                                        
+print(both.hist)      
+
+#12. Applying cOnfidence intervals
+psych::r.con(r=.30,n=50)
+psych::r.con(r=.30,n=500)
+psych::r.con(r=.30,n=1000)
+#0.02362508 0.53377519
+#0.2180456 0.3777530
+#0.2425172 0.3553837
+
+pwr.r.test(r=.35,power=.80,alternative="two.sided")
+#N=60.93514
+##take with a grain of salt - traditional power analysis assumes population correlation is known
+##thus if population correlation is lower but caused this sample r, this power analysis would 
+##vastly underestimate the number of participants needed to obtain a power of .8
+
+
+#solution = SAFEGUARD POWER ANALYSIS 
+psych::r.con(r=.35,n=100)
+#0.1649195 0.5112702
+#Then use the lower limit (lowest probable parameter) and calculate power analysis using this 
+pwr.r.test(r=.01649, power=.8, alternative="two.sided")
+#N = 28861.45
+
+#DOING IT YOURSELF
+##Assume you're looking into a new field of research with ONE study. N=70, r=.41
+psych::r.con(r=.41,n=70)
+pwr.r.test(r=.1937, power=.8, alternative = "two.sided")
+
+pi.r(r=.41,n=70,rep.n=214)
+#95% CI[.19,.59], 95% PI [.17,.62]
+#Indicating that the population correlation responsible for the r=.41 correlation could be as low 
+##as p=.19 or as high as .59. But due to sampling error, you get a replication correlation out 
+##of this range as indicated by the prediction interval. Thus, replication correlation can be 
+##as low as r=.17 or as high as r=.62. 
+
+#The intervals are wide because original study used a small sample size. 
